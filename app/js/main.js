4dd7215c7839bf5ea08dbe129911934c6874411c
Evgeniy1979 <buriyakovskiy.evgen@gmail.com>
2021-03-07 21:52:29 +0200

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function() {
+	$(".rate-star").rateYo( {
+		starWidth: "12px",
+		readOnly: true,
+    rating: 5
+  });
+	$('.product-slider__inner').slick( {
+		dots: true,
+		arrows: false,
+		slidesToShow: 4,
+  	slidesToScroll: 4
+	});
 	var mixer = mixitup('.products__inner-box');
 });
